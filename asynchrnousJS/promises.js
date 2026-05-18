733a8de8f629b71a717060d6bc12f0ea4c001996
kev-disie <disiekelvin@gmail.com>
2026-05-18 14:24:33 +0300

    Idea.png

--- asynchrnousJS/promises.js
+++ asynchrnousJS/promises.js
@@ -1,94 +1,87 @@
-//promises
-
-const promised = new Promise((resolve,reject) =>{
-    const allWentWell = true;
-    if (allWentWell){
-        resolve("All things will be well")
-
-    }else{
-        reject("Yap!I'll always be there for Us.")
-    }
-
-})
-
-//console.log(promised)
-
-promised
-    .then((value)=>{
-        console.log( value);
-    })
-    .catch((reason)=>{
-        console.log(reason);
-    })
-
-const prom = new Promise ((resolve, reject) =>{
-    const randomNumber = Math.floor(Math.random() * 10)
-    console.log(randomNumber)
-    setTimeout(()=>{
-        if (randomNumber < 4){
-            resolve("Well done you guessed it right!")
-        }else {
-            reject("Opps! you guessed it wrong!")
-        }
-    },3000)
-})
-
-//console.log(prom)
-
-prom
-    .then((value)=>{
-    console.log(value)
-})
-    .catch((reason)=>{
-    console.log(reason)
-})
-
-const promiseOne = new Promise((resolve) =>{
-    resolve("Promise one has been resolved")
-})
-const promiseTwo = new Promise((resolve) =>{
-    resolve("Promise two has been resolved");
-})
-const promiseThree = new Promise((reject) =>{
-    reject("Promise three has been rejected.Unlucky you!!");
-})
-
-promiseOne
-    .then((value)=>{
-        console.log(value)
-        return promiseTwo
-    })
-    .then((value)=>{
-        console.log(value)
-        return promiseThree;
-    })
-    .catch((error)=>{
-        console.log(error)
-
-    })
+// //promises
+//
+// const promised = new Promise((resolve,reject) =>{
+//     const allWentWell = true;
+//     if (allWentWell){
+//         resolve("All things will be well")
+//
+//     }else{
+//         reject("Yap!I'll always be there for Us.")
+//     }
+//
+// })
+//
+// //console.log(promised)
+//
+// promised
+//     .then((value)=>{
+//         console.log( value);
+//     })
+//     .catch((reason)=>{
+//         console.log(reason);
+//     })
+//
+// const prom = new Promise ((resolve, reject) =>{
+//     const randomNumber = Math.floor(Math.random() * 10)
+//     console.log(randomNumber)
+//     setTimeout(()=>{
+//         if (randomNumber < 4){
+//             resolve("Well done you guessed it right!")
+//         }else {
+//             reject("Opps! you guessed it wrong!")
+//         }
+//     },3000)
+// })
+//
+// //console.log(prom)
+//
+// prom
+//     .then((value)=>{
+//     console.log(value)
+// })
+//     .catch((reason)=>{
+//     console.log(reason)
+// })
+//
+// const promiseOne = new Promise((resolve) =>{
+//     resolve("Promise one has been resolved")
+// })
+// const promiseTwo = new Promise((resolve) =>{
+//     resolve("Promise two has been resolved");
+// })
+// const promiseThree = new Promise((reject) =>{
+//     reject("Promise three has been rejected.Unlucky you!!");
+// })
+//
+// promiseOne
+//     .then((value)=>{
+//         console.log(value)
+//         return promiseTwo
+//     })
+//     .then((value)=>{
+//         console.log(value)
+//         return promiseThree;
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//
+//     })
 
 //.all method checking all promises fulfilled else fail
 
 const IOU = new Promise((resolve) =>{
-    resolve("IOU one  has been resolved")
+
+   setTimeout(()=>{
+       resolve("IOU one  has been resolved")
+   },2000)
 })
 const IOUTwo = new Promise((resolve) =>{
-    resolve("IOU Two has been resolved")
-})
-const IOUThree = new Promise(( resolve,reject)=>{
-    reject("IOUThree has failed therefore do not expect any magic!!")
+    setTimeout(()=>{
+        resolve("IOU Two has been resolved")
+    })
 })
 
-Promise.all([IOU,IOUTwo,IOUThree])
-    .then((value)=>
-    {
-        console.log(value)
-        return value[1];
-    })
-    .then((value)=>{
-        console.log(value)
-        return value[2];
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+Promise.all([IOU,IOUTwo])
+    .then((data)=>console.log(data[0],data[1]))
+    .catch((error)=>{console.log("There is running away from your fate!!")
+error()})
